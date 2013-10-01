8c8504213d965eafcb43aaa7cf927cc7d1423f50
Jonas Baumann <jone@jone.ch>
2013-10-01 20:01:49 +0200

    Create package.

---
+++ README.rst
@@ -0,0 +1,29 @@
+ftw.browser
+===========
+
+.. contents:: Table of Contents
+
+
+Motivation
+----------
+
+
+Installation and usage
+----------------------
+
+
+Links
+-----
+
+- Main github project repository: https://github.com/4teamwork/ftw.browser
+- Issue tracker: https://github.com/4teamwork/ftw.browser/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.browser
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.browser
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.browser`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.dev0'
+
+
+tests_require = [
+    'unittest2',
+    ]
+
+
+setup(name='ftw.browser',
+      version=version,
+      description='A test browser for Zope and Plone.',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw browser testbrowser test',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.browser',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
