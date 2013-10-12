1700be9b4a99b84e7fb2306a68697341c6a7a115
Jonas Baumann <jone@jone.ch>
2013-10-12 18:23:46 +0200

    Add test for testing that browser sessions works properly.

---
+++ ftw/browser/tests/test_session.py
@@ -0,0 +1,25 @@
+from ftw.browser import browsing
+from ftw.browser.pages import plone
+from plone.app.testing import PLONE_FUNCTIONAL_TESTING
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+from unittest2 import TestCase
+
+
+class TestBrowserSession(TestCase):
+
+    layer = PLONE_FUNCTIONAL_TESTING
+
+    @browsing
+    def test_browser_stays_logged_in(self, browser):
+        browser.open()
+        self.assertFalse(plone.logged_in())
+
+        browser.visit(view='login_form')
+        browser.fill({'Login Name': TEST_USER_NAME,
+                      'Password': TEST_USER_PASSWORD}).submit()
+        self.assertEquals(TEST_USER_ID, plone.logged_in())
+
+        browser.visit(view='/')
+        self.assertEquals(TEST_USER_ID, plone.logged_in())
