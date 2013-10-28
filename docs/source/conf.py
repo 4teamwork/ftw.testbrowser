8c885f1b614054d5f6d9ed082c5e89ca89968eea
Jonas Baumann <jone@jone.ch>
2013-10-28 10:28:00 +0100

    Use sphinx theme "nature".

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@ copyright = u'2013, 4teamwork GmbH'
 
 extensions = ['sphinx.ext.autodoc']
 master_doc = 'index'
+html_theme = 'nature'
 
 from pkg_resources import get_distribution
 version = release = get_distribution(project).version
