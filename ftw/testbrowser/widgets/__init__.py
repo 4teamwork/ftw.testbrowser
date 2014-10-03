d03eced8ee32d3852d9df7cddb473acae02889cb
Jonas Baumann <jone@jone.ch>
2014-10-03 14:15:18 +0200

    Widgets: test for sequence widget after testing for autocomplete widgets.
    Some widgets match both, autocomplete and sequence widgets.
    In this case we want to have the autocomplete widget.
    
    Warning: the widgets modules need to be imported in reverse.

--- ftw/testbrowser/widgets/__init__.py
+++ ftw/testbrowser/widgets/__init__.py
@@ -1,6 +1,6 @@
-from ftw.testbrowser.widgets import autocomplete
 from ftw.testbrowser.widgets import datetime
 from ftw.testbrowser.widgets import file
 from ftw.testbrowser.widgets import sequence
+from ftw.testbrowser.widgets import autocomplete
 from ftw.testbrowser.widgets import atmultiselect
 from ftw.testbrowser.widgets import z3cchoicecollection
