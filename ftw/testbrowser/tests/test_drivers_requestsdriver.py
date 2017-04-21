c031885c5371e57ec29c55b575e2d155f8594472
Jonas Baumann <jone@jone.ch>
2017-04-21 09:25:40 +0200

    tests: fix requests driver testing layer.

--- ftw/testbrowser/tests/test_drivers_requestsdriver.py
+++ ftw/testbrowser/tests/test_drivers_requestsdriver.py
@@ -1,23 +1,20 @@
 from ftw.testbrowser import browsing
-from ftw.testbrowser.core import LIB_REQUESTS
 from ftw.testbrowser.drivers.requestsdriver import RequestsDriver
 from ftw.testbrowser.interfaces import IDriver
-from ftw.testbrowser.testing import DEFAULT_TESTING
+from ftw.testbrowser.testing import REQUESTS_TESTING
 from ftw.testbrowser.tests import FunctionalTestCase
 from zope.interface.verify import verifyClass
 
 
 class TestRequestsDriverImplementation(FunctionalTestCase):
-    layer = DEFAULT_TESTING
+    layer = REQUESTS_TESTING
 
     def test_implements_interface(self):
         verifyClass(IDriver, RequestsDriver)
 
     @browsing
     def test_does_not_support_duplicate_request_headers(self, browser):
-        browser.request_library = LIB_REQUESTS
         browser.get_driver()
-
         browser.append_request_header('Authorization', 'Basic foo:foo')
 
         with self.assertRaises(NameError) as cm:
