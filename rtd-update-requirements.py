41cc1bb7f0294d3f8fc51923ffc4ddc7f67802d7
Jonas Baumann <jone@jone.ch>
2017-06-23 14:48:50 +0200

    Do not pin eggs used by read the docs.

--- rtd-update-requirements.py
+++ rtd-update-requirements.py
@@ -16,9 +16,22 @@ with open(sphinx_build_path, 'r') as sphinx_build_fio:
        if match:
           requirements.append(match.groups())
 
+# These are the eggs that are installed by read the docs. Rtd installs specific
+# versions. When we install other versions it may not work with the commands
+# rtd runs. Therefore we must not pin those packages.
+# Copied form the build's "pip install".
+rtd_eggs = map(str.lower, map(str.strip, (
+   'six, idna, urllib3, chardet, certifi, requests, docutils,'
+   ' Pygments, imagesize, pytz, babel, snowballstemmer, MarkupSafe,'
+   ' Jinja2, alabaster, sphinx, singledispatch, backports-abc, tornado,'
+   ' click, livereload, mkdocs-bootstrap, mkdocs-bootswatch, PyYAML,'
+   ' Markdown, mkdocs, mock, pillow, nilsimsa, readthedocs-sphinx-ext,'
+   ' sphinx-rtd-theme, commonmark, recommonmark').split(',')))
 
 with open(requirements_path, 'w+') as requirements_fio:
    for name, version in requirements:
+      if name.lower() in rtd_eggs:
+         continue
       requirements_fio.write('{} == {}\n'.format(name, version))
 
 print 'Updated.'
