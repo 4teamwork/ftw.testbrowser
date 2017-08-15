e5f0526e56c0a060b95646a0af217452c5d21d73
David Erni <david.erni@4teamwork.ch>
2017-08-15 12:01:28 +0200

    Use current pages default `#form` in erroneous_fields.
    Use the form identified by the id `#form` as the default form for
    the erroneous_fields page object. For plone tests it can be assumed
    that often the form under test is a content-type related form. Both
    add and edit forms of plone content-types use the id `#form`. Thus we
    provide this shortcut.

--- ftw/testbrowser/tests/test_pages_z3cform.py
+++ ftw/testbrowser/tests/test_pages_z3cform.py
@@ -9,7 +9,7 @@ from ftw.testbrowser.tests.alldrivers import all_drivers
 class TestZ3cformPageObject(BrowserTestCase):
 
     @browsing
-    def test_erroneous_fields(self, browser):
+    def test_erroneous_fields_default_form(self, browser):
         self.grant('Manager')
 
         browser.login().open()
@@ -18,6 +18,23 @@ class TestZ3cformPageObject(BrowserTestCase):
 
         self.assertEquals(browser.previous_url, browser.url)
 
-        form = browser.css('form#form').first
         self.assertEquals({u'Title': ['Required input is missing.']},
-                          z3cform.erroneous_fields(form))
+                          z3cform.erroneous_fields())
+
+    @browsing
+    def test_erroneous_fields_missing_default_form(self, browser):
+        browser.visit(view='test-form')
+        with self.assertRaises(ValueError) as cm:
+            z3cform.erroneous_fields()
+
+        self.assertEqual(
+            "Could not find the default `#form` on the current page, please "
+            "provide a valid form. Available forms are: "
+            "['test-form', 'searchGadget_form', 'test-get-form']",
+            cm.exception.message)
+
+    @browsing
+    def test_erroneous_fields_with_custom_form(self, browser):
+        browser.visit(view='test-form')
+        self.assertEqual(
+            {}, z3cform.erroneous_fields(browser.forms['test-form']))
