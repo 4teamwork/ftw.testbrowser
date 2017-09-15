661225b066e0a70723ee4669f4c7f326f8621e7f
Jonas Baumann <jone@jone.ch>
2017-09-15 09:45:31 +0200

    Plone page object: test with cloned browser.

--- ftw/testbrowser/tests/test_pages_plone.py
+++ ftw/testbrowser/tests/test_pages_plone.py
@@ -1,8 +1,8 @@
-from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.pages import plone
 from ftw.testbrowser.tests import BrowserTestCase
 from ftw.testbrowser.tests.alldrivers import all_drivers
+from ftw.testbrowser.tests.helpers import nondefault_browsing
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import TEST_USER_ID
 
@@ -10,56 +10,56 @@ from plone.app.testing import TEST_USER_ID
 @all_drivers
 class TestPlonePageObject(BrowserTestCase):
 
-    @browsing
+    @nondefault_browsing
     def test_not_logged_in(self, browser):
         browser.open(self.portal.portal_url())
-        self.assertFalse(plone.logged_in())
+        self.assertFalse(plone.logged_in(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_logged_in(self, browser):
         browser.login().open(self.portal.portal_url())
-        self.assertEquals(TEST_USER_ID, plone.logged_in())
+        self.assertEquals(TEST_USER_ID, plone.logged_in(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_view_on_root(self, browser):
         browser.open()
-        self.assertEquals('listing_view', plone.view())
+        self.assertEquals('listing_view', plone.view(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_view_on_login_form(self, browser):
         browser.open(view='login_form')
-        self.assertEquals('login_form', plone.view())
+        self.assertEquals('login_form', plone.view(browser=browser))
 
 
-    @browsing
+    @nondefault_browsing
     def test_portal_type(self, browser):
         browser.open()
-        self.assertEquals('plone-site', plone.portal_type())
+        self.assertEquals('plone-site', plone.portal_type(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_view_and_portal_type(self, browser):
         browser.open()
         self.assertEquals(('listing_view', 'plone-site'),
-                          plone.view_and_portal_type())
+                          plone.view_and_portal_type(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_first_heading(self, browser):
         browser.open()
-        self.assertEquals('Plone site', plone.first_heading())
+        self.assertEquals('Plone site', plone.first_heading(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_first_heading_on_at_addform(self, browser):
         browser.login(SITE_OWNER_NAME).open()
-        factoriesmenu.add('Folder')
-        self.assertEquals('Add Folder', plone.first_heading())
+        factoriesmenu.add('Folder', browser=browser)
+        self.assertEquals('Add Folder', plone.first_heading(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_document_description(self, browser):
         browser.login(SITE_OWNER_NAME).open(view='overview-controlpanel')
         self.assertEquals('Configuration area for Plone and add-on Products.',
-                          plone.document_description())
+                          plone.document_description(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_no_document_description(self, browser):
         browser.open()
-        self.assertEquals(None, plone.document_description())
+        self.assertEquals(None, plone.document_description(browser=browser))
