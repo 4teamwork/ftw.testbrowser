d14c2ecce439cc51318229d3e46eaf9d57cffb50
Jonas Baumann <jone@jone.ch>
2017-09-15 09:46:59 +0200

    z3cform page object: improve multi browser support.

--- ftw/testbrowser/tests/test_pages_z3cform.py
+++ ftw/testbrowser/tests/test_pages_z3cform.py
@@ -1,23 +1,23 @@
-from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.pages import z3cform
 from ftw.testbrowser.tests import BrowserTestCase
 from ftw.testbrowser.tests.alldrivers import all_drivers
+from ftw.testbrowser.tests.helpers import nondefault_browsing
 
 
 @all_drivers
 class TestZ3cformPageObject(BrowserTestCase):
 
-    @browsing
+    @nondefault_browsing
     def test_erroneous_fields(self, browser):
         self.grant('Manager')
 
         browser.login().open()
-        factoriesmenu.add('Folder')
+        factoriesmenu.add('Folder', browser=browser)
         browser.find('Save').click()
 
         self.assertEquals(browser.previous_url, browser.url)
 
         form = browser.css('form#form').first
         self.assertEquals({u'Title': ['Required input is missing.']},
-                          z3cform.erroneous_fields(form))
+                          z3cform.erroneous_fields(form, browser=browser))
