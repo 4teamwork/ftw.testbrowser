61e9c0c1fd019354809a21cd602b3d3d282a7817
Jonas Baumann <jone@jone.ch>
2017-09-15 09:42:24 +0200

    Factories page object: test with cloned browser.

--- ftw/testbrowser/tests/test_pages_factoriesmenu.py
+++ ftw/testbrowser/tests/test_pages_factoriesmenu.py
@@ -1,65 +1,65 @@
 from ftw.builder import Builder
 from ftw.builder import create
-from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.tests import BrowserTestCase
 from ftw.testbrowser.tests.alldrivers import all_drivers
+from ftw.testbrowser.tests.helpers import nondefault_browsing
 
 
 @all_drivers
 class TestFactoriesMenu(BrowserTestCase):
 
-    @browsing
+    @nondefault_browsing
     def test_factoriesmenu_visible(self, browser):
         self.grant('Manager')
         browser.open()
-        self.assertFalse(factoriesmenu.visible())
+        self.assertFalse(factoriesmenu.visible(browser=browser))
         browser.login().open()
-        self.assertTrue(factoriesmenu.visible())
+        self.assertTrue(factoriesmenu.visible(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_addable_types(self, browser):
         self.grant('Manager')
         browser.login().open()
-        self.assertIn('Folder', factoriesmenu.addable_types())
+        self.assertIn('Folder', factoriesmenu.addable_types(browser=browser))
 
-    @browsing
+    @nondefault_browsing
     def test_addable_types_raises_when_menu_not_visible(self, browser):
         browser.open()
         with self.assertRaises(ValueError) as cm:
-            factoriesmenu.addable_types()
+            factoriesmenu.addable_types(browser=browser)
 
         self.assertEquals('Factories menu is not visible.', str(cm.exception))
 
-    @browsing
+    @nondefault_browsing
     def test_adding_content(self, browser):
         self.grant('Manager')
         browser.login().open()
-        factoriesmenu.add('Folder')
+        factoriesmenu.add('Folder', browser=browser)
         self.assertEquals(self.portal.portal_url() + '/++add++Folder', browser.url)
 
-    @browsing
+    @nondefault_browsing
     def test_adding_unallowed_or_missing_type(self, browser):
         self.grant('Manager')
         browser.login().open()
         with self.assertRaises(ValueError) as cm:
-            factoriesmenu.add('Unkown Type')
+            factoriesmenu.add('Unkown Type', browser=browser)
 
         self.assertTrue(
             str(cm.exception).startswith(
                 'The type "Unkown Type" is not addable. Addable types: '),
             str(cm.exception))
 
-    @browsing
+    @nondefault_browsing
     def test_adding_fails_when_no_factoriesmenu_visible(self, browser):
         browser.open()
         with self.assertRaises(ValueError) as cm:
-            factoriesmenu.add('Folder')
+            factoriesmenu.add('Folder', browser=browser)
 
         self.assertEquals('Cannot add "Folder": no factories menu visible.',
                           str(cm.exception))
 
-    @browsing
+    @nondefault_browsing
     def test_addable_types_works_with_restrictions_entry(self, browser):
         self.grant('Manager')
         # Regression:
@@ -68,4 +68,5 @@ class TestFactoriesMenu(BrowserTestCase):
         # This test verifies that this still works.
         folder = create(Builder('folder'))
         browser.login().visit(folder)
-        self.assertIn(u'Restrictions\u2026', factoriesmenu.addable_types())
+        self.assertIn(u'Restrictions\u2026',
+                      factoriesmenu.addable_types(browser=browser))
