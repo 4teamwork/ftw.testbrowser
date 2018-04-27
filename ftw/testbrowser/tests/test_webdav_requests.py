0b3458f7e5cb86a7240665a6a2e090d5fe76f8df
Niklaus Johner <niklaus.johner@4teamwork.ch>
2018-04-27 09:30:19 +0200

    Workaround for webdav xml namespacing issue

--- ftw/testbrowser/tests/test_webdav_requests.py
+++ ftw/testbrowser/tests/test_webdav_requests.py
@@ -1,3 +1,5 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser import browsing
 from ftw.testbrowser.core import LIB_MECHANIZE
 from ftw.testbrowser.exceptions import NoWebDAVSupport
@@ -6,6 +8,8 @@ from ftw.testbrowser.testing import MECHANIZE_TESTING
 from ftw.testbrowser.tests import BrowserTestCase
 from ftw.testbrowser.tests.alldrivers import all_drivers
 from ftw.testbrowser.tests.alldrivers import skip_driver
+from lxml import etree
+from plone.app.testing import SITE_OWNER_NAME
 
 
 @all_drivers
@@ -37,6 +41,28 @@ class TestWebdavRequests(BrowserTestCase):
         browser.login().webdav('PROPFIND', data=data)
         self.assertEquals('Plone site',
                           browser.xpath('//d:displayname').first.text)
+
+    @skip_driver(LIB_MECHANIZE, 'Mechanize does not support webdav.')
+    @browsing
+    def test_browser_corrects_webdav_lock_namespacing_bug(self, browser):
+        data = (
+            '<?xml version="1.0" encoding="utf-8"?>\n'
+            '<D:lockinfo xmlns:D="DAV:">\n'
+            '  <D:lockscope><D:exclusive/></D:lockscope>\n'
+            '  <D:locktype><D:write/></D:locktype>\n'
+            '  <D:owner>\n'
+            '  <D:href>Test Token</D:href>\n'
+            '  </D:owner>\n'
+            '</D:lockinfo>'
+            )
+        browser.login(SITE_OWNER_NAME).open()
+        document = create(Builder('document').titled(u'Foo'))
+        browser.login(SITE_OWNER_NAME).webdav('LOCK', document, data=data)
+        # response contains wrong namespace for the lock token
+        self.assertIn("<D:href>Test Token</D:href>", browser.contents)
+        # this is corrected in parsed contents
+        self.assertIn("<d:href>Test Token</d:href>", etree.tostring(browser.document))
+        self.assertEqual("Test Token", browser.document.find('.//{DAV:}href').text)
 
 
 class TestNoZserverWebdavRequests(BrowserTestCase):
