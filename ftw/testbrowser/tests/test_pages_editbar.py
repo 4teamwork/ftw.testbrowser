53b23d4f0d8b296297a9f47fd36e6ba95a5d4baa
Daniel Jowett <d.jowett@4teamwork.ch>
2020-07-27 15:59:16 +0200

    Switch TestEditBar tests to test folders rather than PloneSiteRoot

--- ftw/testbrowser/tests/test_pages_editbar.py
+++ ftw/testbrowser/tests/test_pages_editbar.py
@@ -1,3 +1,5 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.testbrowser.exceptions import NoElementFound
 from ftw.testbrowser.pages import editbar
 from ftw.testbrowser.tests import BrowserTestCase
@@ -10,6 +12,10 @@ import re
 
 @all_drivers
 class TestEditBar(BrowserTestCase):
+
+    def setUp(self):
+        super(TestEditBar, self).setUp()
+        self.folder = create(Builder('folder').titled(u'The Folder'))
 
     @nondefault_browsing
     def test_visible(self, browser):
@@ -57,18 +63,18 @@ class TestEditBar(BrowserTestCase):
     @nondefault_browsing
     def test_menus(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         if IS_PLONE_4:
             self.assertEqual([u'Add new', 'Display'],
                              editbar.menus(browser=browser))
         else:
-            self.assertEqual([u'Add new', 'Display', 'Manage portlets'],
+            self.assertEqual([u'Add new', 'Actions', 'Display', 'Manage portlets'],
                              editbar.menus(browser=browser))
 
     @nondefault_browsing
     def test_menu(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         if IS_PLONE_4:
             self.assertIn('actionMenu', editbar.menu('Display', browser=browser).classes)
             self.assertIn('actionMenu', editbar.menu('Add new', browser=browser).classes)
@@ -81,7 +87,7 @@ class TestEditBar(BrowserTestCase):
     @nondefault_browsing
     def test_menu_not_found(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         with self.assertRaises(NoElementFound) as cm:
             editbar.menu('Shapes', browser=browser)
 
@@ -95,19 +101,19 @@ class TestEditBar(BrowserTestCase):
             self.assertEqual(
                 "Empty result set: editbar.menu('Shapes',"
                 " browser=<ftw.browser.core.Browser instance>) did not match any nodes."
-                "\nVisible menus: ['Add new', 'Display', 'Manage portlets'].",
+                "\nVisible menus: ['Add new', 'Actions', 'Display', 'Manage portlets'].",
                 str(cm.exception))
 
     @nondefault_browsing
     def test_menu_options(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         self.assertIn('Summary view', editbar.menu_options('Display', browser=browser))
 
     @nondefault_browsing
     def test_menu_option(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         link = editbar.menu_option('Display', 'Summary view', browser=browser)
         self.assertEqual('plone-contentmenu-display-summary_view',
                          link.attrib.get('id'))
@@ -116,7 +122,7 @@ class TestEditBar(BrowserTestCase):
     @nondefault_browsing
     def test_menu_option_not_found(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         with self.assertRaises(NoElementFound) as cm:
             editbar.menu_option('Add new', 'Dog', browser=browser)
 
@@ -125,13 +131,13 @@ class TestEditBar(BrowserTestCase):
             " browser=<ftw.browser.core.Browser instance>)"
             " did not match any nodes."
             "\nOptions in menu 'Add new': ['Collection', 'DXType', 'Event',"
-            " 'File', 'Folder', 'Image', 'Link', 'News Item', 'Page']",
+            " 'File', 'Folder', 'Image', 'Link', 'News Item', 'Page', u'Restrictions\u2026']",
             str(cm.exception))
 
     @nondefault_browsing
     def test_menu_option_menu_not_found(self, browser):
         self.grant('Manager')
-        browser.login().open()
+        browser.login().open(self.folder)
         with self.assertRaises(NoElementFound) as cm:
             editbar.menu_option('Shapes', 'Square', browser=browser)
 
@@ -147,7 +153,7 @@ class TestEditBar(BrowserTestCase):
                 "Empty result set: editbar.menu_option('Shapes', 'Square',"
                 " browser=<ftw.browser.core.Browser instance>)"
                 " did not match any nodes."
-                "\nVisible menus: ['Add new', 'Display', 'Manage portlets'].",
+                "\nVisible menus: ['Add new', 'Actions', 'Display', 'Manage portlets'].",
                 str(cm.exception))
 
     @nondefault_browsing
